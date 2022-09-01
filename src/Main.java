7d8818573a253c49a9babd6e980d80266d71d45e
Станислав <zurlov.s.s@outlook.com>
2022-09-01 15:09:27 +0300

    Переменные Урок 2

--- src/Main.java
+++ src/Main.java
@@ -61,6 +61,95 @@ public class Main {
         + " часов работы может быть поделено между сотрудниками.");
 
 
+        // Переменные 2
+        System.out.println("Переменные 2.");
+
+        int i = 748; // Целочисленные
+        byte b = 126;
+        short s = 67;
+        long l = 92233l;
+
+        float f = 1.5f;  // Дробные
+        double d = 6.85464;
+        System.out.println("Значение переменной : " + "i = " + i + " ,b = " +b + " ,s = " + s + ",l = " + l
+                + " ,f = " + f + " ,d = " + d);
+
+        byte LyudmilaPavlovna = 23;
+        byte AnnaSergeyevna = 27;
+        byte EkaterinaAndreevna = 30;
+        short TotalSheetsOfPaperPurchased = 480;
+        var TotalStudents = LyudmilaPavlovna + AnnaSergeyevna + EkaterinaAndreevna;
+        var OneStudents = TotalSheetsOfPaperPurchased / TotalStudents;
+        System.out.println("На каждого ученика расчитано = " + OneStudents + " листов бумаги");
+
+        var MachinePerformanceInTwoMinutes = 16;
+        var NumberOfBottlesPerMinute = MachinePerformanceInTwoMinutes / 2;
+        System.out.println("Производительность за 1 минуту = " + NumberOfBottlesPerMinute + " бутылок");
+        var SpecifiedPeriodOfTime = 20 * 8;
+        var SetTimeInterval_1Day = 24 * 60 * 8;
+        var SetPeriodOfTime_3days = 3 * 24 * 60 * 8;
+        var SetPeriodOfTime_1month = 30 * 24 * 60 * 8;
+        System.out.println ("За 20 минут машина произвела = " + SpecifiedPeriodOfTime + " бутылок");
+        System.out.println ("За сутки машина произвела = " + SetTimeInterval_1Day + " бутылок");
+        System.out.println ("За 3 дня машина произвела = " + SetPeriodOfTime_3days + " бутылок");
+        System.out.println ("За 1 месяц машина произвела = " + SetPeriodOfTime_1month + " бутылок");
+
+        byte TotalCansForSchoolRenovation = 120;
+        byte PerClassOfWhitePaint = 2;
+        byte ForOneClassOfBrownPaint = 4;
+        byte SumOfOneClass = 6;
+        int TotalClassesInTheSchool = TotalCansForSchoolRenovation / SumOfOneClass;
+        int AllWhitePaint = TotalClassesInTheSchool * PerClassOfWhitePaint;
+        int AllBrownPaint = TotalClassesInTheSchool * ForOneClassOfBrownPaint;
+        System.out.println("В школе, где " + TotalClassesInTheSchool + " классов, нужно "
+                + AllWhitePaint + " банок белой краски и " + AllBrownPaint + " банок коричневой краски");
+
+        float NumberOfBananaPieces = 5 * 80;
+        float AmountOfMilkMl = 2 * 105;
+        float NumberOfIceCreamPieces = 2 * 100;
+        float NumberOfEggs = 4 * 70;
+        float ToKilograms = (NumberOfBananaPieces + AmountOfMilkMl + NumberOfIceCreamPieces + NumberOfEggs) /1000;
+        System.out.println ("Общий вес завтрака = " + ToKilograms + " кг");
+
+        short NeedToDropGram = 7 * 1000;
+        System.out.println("Для участия в соревнованиях, необходимо сбросить = " + NeedToDropGram + " грамм");
+        short Diet_1 = 250;
+        short Diet_2 = 500;
+        int NumberOfDietDays_1 = NeedToDropGram / Diet_1;
+        int NumberOfDietDays_2 = NeedToDropGram / Diet_2;
+        System.out.println("Количество дней с рационом питания_1 = " + NumberOfDietDays_1 +
+        " ,Количество дней с рационом питания_2 = " + NumberOfDietDays_2);
+
+        int CarsCurrentSalary = 67760;
+        int CarsCurrentDenis =83690;
+        int CarsCurrentChistina = 76230;
+        byte OneYear = 12;
+        int SalaryAfterAnnualIncreaseMasha = 67760 * 100 /1000 + 67760;
+        int SalaryAfterAnnualIncreaseDenis = 83690 * 100 /1000 + 83690;
+        int SalaryAfterAnnualIncreaseChristina = 76230 *100 /1000 + 76230;
+        int NewAnnualIncomeMasha = SalaryAfterAnnualIncreaseMasha * OneYear;
+        int NewAnnualIncomeDenis = SalaryAfterAnnualIncreaseDenis * OneYear;
+        int NewAnnualIncomeChristina = SalaryAfterAnnualIncreaseChristina * OneYear;
+        int CurrentAnnualIncomeMasha = CarsCurrentSalary * OneYear;
+        int CurrentAnnualIncomeDenis = CarsCurrentDenis * OneYear;
+        int CurrentAnnualIncomeChristina = CarsCurrentChistina * OneYear;
+        int DifferenceInAnnualIncomeMasha = NewAnnualIncomeMasha - CurrentAnnualIncomeMasha;
+        int DifferenceInAnnualIncomeDenis = NewAnnualIncomeDenis - CurrentAnnualIncomeDenis;
+        int DifferenceInAnnualIncomeChristina = NewAnnualIncomeChristina - CurrentAnnualIncomeChristina;
+        System.out.println ("Ежемесячная зарплата с повышением на 10% от текущей зарплаты составила: Маша = "
+        + SalaryAfterAnnualIncreaseMasha + " ,Денис = " + SalaryAfterAnnualIncreaseDenis + " ,Кристина = " + SalaryAfterAnnualIncreaseChristina);
+        System.out.println("Разница между годовым доходом до и после повышения: Маша = " + DifferenceInAnnualIncomeMasha
+        + " ,Денис = " + DifferenceInAnnualIncomeDenis + " , Кристина = " + DifferenceInAnnualIncomeChristina);
+        // Все задачи выполнены
+
+
+
+
+
+
+
+
+
 
 
 
